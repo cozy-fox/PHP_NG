e95e8d13e05d5218a84707205df9cbf1b0e9380f
peterpaker48 <peterpaker611@gmail.com>
2016-04-09 18:13:11 +0800

    [src] fixe: nothing response error

--- src/ngx_http_php_handler.c
+++ src/ngx_http_php_handler.c
@@ -56,43 +56,11 @@ ngx_http_php_rewrite_file_handler(ngx_http_request_t *r)
 		ngx_php_ngx_run(r, pmcf->state, plcf->rewrite_code);
 	NGX_HTTP_PHP_NGX_SHUTDOWN;
 
-	ngx_int_t rc;
-
-	ngx_http_php_rputs_chain_list_t *chain;
-	
 	ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
-	chain = ctx->rputs_chain;
-	if (chain == NULL){
-		return NGX_ERROR;
-	}
-
-	r->headers_out.content_type.len = sizeof("text/html") - 1;
-	r->headers_out.content_type.data = (u_char *)"text/html";
-	if (!r->headers_out.status){
-		r->headers_out.status = NGX_HTTP_OK;
-	}
-
-	if (r->method == NGX_HTTP_HEAD){
-		rc = ngx_http_send_header(r);
-		if (rc != NGX_OK){
-			return rc;
-		}
-	}
-
-	if (chain != NULL){
-		(*chain->last)->buf->last_buf = 1;
-	}
-
-	rc = ngx_http_send_header(r);
-	if (rc != NGX_OK){
-		return rc;
-	}
-
-	ngx_http_output_filter(r, chain->out);
 
 	ngx_http_set_ctx(r, NULL, ngx_http_php_module);
 
-	return NGX_OK;
+	return NGX_HTTP_SPECIAL_RESPONSE;
 }
 
 ngx_int_t 
@@ -131,43 +99,11 @@ ngx_http_php_rewrite_inline_handler(ngx_http_request_t *r)
 		ngx_php_ngx_run(r, pmcf->state, plcf->rewrite_inline_code);
 	NGX_HTTP_PHP_NGX_SHUTDOWN;
 
-	ngx_int_t rc;
-
-	ngx_http_php_rputs_chain_list_t *chain;
-	
 	ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
-	chain = ctx->rputs_chain;
-	if (chain == NULL){
-		return NGX_ERROR;
-	}
-
-	r->headers_out.content_type.len = sizeof("text/html") - 1;
-	r->headers_out.content_type.data = (u_char *)"text/html";
-	if (!r->headers_out.status){
-		r->headers_out.status = NGX_HTTP_OK;
-	}
-
-	if (r->method == NGX_HTTP_HEAD){
-		rc = ngx_http_send_header(r);
-		if (rc != NGX_OK){
-			return rc;
-		}
-	}
-
-	if (chain != NULL){
-		(*chain->last)->buf->last_buf = 1;
-	}
-
-	rc = ngx_http_send_header(r);
-	if (rc != NGX_OK){
-		return rc;
-	}
-
-	ngx_http_output_filter(r, chain->out);
 
 	ngx_http_set_ctx(r, NULL, ngx_http_php_module);
 
-	return NGX_OK;
+	return NGX_HTTP_SPECIAL_RESPONSE;
 }
 
 ngx_int_t 
@@ -339,8 +275,34 @@ ngx_http_php_content_file_handler(ngx_http_request_t *r)
 	
 	ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
 	chain = ctx->rputs_chain;
-	if (chain == NULL){
-		return NGX_ERROR;
+	
+	if (ctx->rputs_chain == NULL){
+		ngx_buf_t *b;
+		ngx_str_t ns;
+		u_char *u_str;
+		ns.data = (u_char *)" ";
+		ns.len = 1;
+		
+		chain = ngx_pcalloc(r->pool, sizeof(ngx_http_php_rputs_chain_list_t));
+		chain->out = ngx_alloc_chain_link(r->pool);
+		chain->last = &chain->out;
+	
+		b = ngx_calloc_buf(r->pool);
+		(*chain->last)->buf = b;
+		(*chain->last)->next = NULL;
+
+		u_str = ngx_pstrdup(r->pool, &ns);
+		u_str[ns.len] = '\0';
+		(*chain->last)->buf->pos = u_str;
+		(*chain->last)->buf->last = u_str + ns.len;
+		(*chain->last)->buf->memory = 1;
+		ctx->rputs_chain = chain;
+
+		if (r->headers_out.content_length_n == -1){
+			r->headers_out.content_length_n += ns.len + 1;
+		}else {
+			r->headers_out.content_length_n += ns.len;
+		}
 	}
 
 	r->headers_out.content_type.len = sizeof("text/html") - 1;
@@ -424,13 +386,38 @@ ngx_http_php_content_inline_handler(ngx_http_request_t *r)
 		ngx_php_ngx_run(r, pmcf->state, plcf->content_inline_code);
 	NGX_HTTP_PHP_NGX_SHUTDOWN;
 
-
 	ngx_http_php_rputs_chain_list_t *chain;
 	
 	ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
 	chain = ctx->rputs_chain;
-	if (chain == NULL){
-		return NGX_ERROR;
+
+	if (ctx->rputs_chain == NULL){
+		ngx_buf_t *b;
+		ngx_str_t ns;
+		u_char *u_str;
+		ns.data = (u_char *)" ";
+		ns.len = 1;
+		
+		chain = ngx_pcalloc(r->pool, sizeof(ngx_http_php_rputs_chain_list_t));
+		chain->out = ngx_alloc_chain_link(r->pool);
+		chain->last = &chain->out;
+	
+		b = ngx_calloc_buf(r->pool);
+		(*chain->last)->buf = b;
+		(*chain->last)->next = NULL;
+
+		u_str = ngx_pstrdup(r->pool, &ns);
+		u_str[ns.len] = '\0';
+		(*chain->last)->buf->pos = u_str;
+		(*chain->last)->buf->last = u_str + ns.len;
+		(*chain->last)->buf->memory = 1;
+		ctx->rputs_chain = chain;
+
+		if (r->headers_out.content_length_n == -1){
+			r->headers_out.content_length_n += ns.len + 1;
+		}else {
+			r->headers_out.content_length_n += ns.len;
+		}
 	}
 
 	r->headers_out.content_type.len = sizeof("text/html") - 1;
@@ -508,13 +495,38 @@ ngx_http_php_content_post_handler(ngx_http_request_t *r)
 		ngx_php_ngx_run(r, pmcf->state, plcf->content_inline_code);
 	NGX_HTTP_PHP_NGX_SHUTDOWN;
 
-
 	ngx_http_php_rputs_chain_list_t *chain;
 	
 	ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
 	chain = ctx->rputs_chain;
-	if (chain == NULL){
-		return NGX_ERROR;
+	
+	if (ctx->rputs_chain == NULL){
+		ngx_buf_t *b;
+		ngx_str_t ns;
+		u_char *u_str;
+		ns.data = (u_char *)" ";
+		ns.len = 1;
+		
+		chain = ngx_pcalloc(r->pool, sizeof(ngx_http_php_rputs_chain_list_t));
+		chain->out = ngx_alloc_chain_link(r->pool);
+		chain->last = &chain->out;
+	
+		b = ngx_calloc_buf(r->pool);
+		(*chain->last)->buf = b;
+		(*chain->last)->next = NULL;
+
+		u_str = ngx_pstrdup(r->pool, &ns);
+		u_str[ns.len] = '\0';
+		(*chain->last)->buf->pos = u_str;
+		(*chain->last)->buf->last = u_str + ns.len;
+		(*chain->last)->buf->memory = 1;
+		ctx->rputs_chain = chain;
+
+		if (r->headers_out.content_length_n == -1){
+			r->headers_out.content_length_n += ns.len + 1;
+		}else {
+			r->headers_out.content_length_n += ns.len;
+		}
 	}
 
 	r->headers_out.content_type.len = sizeof("text/html") - 1;
