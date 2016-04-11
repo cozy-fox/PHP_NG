2580992410262e581731b5b4dda57b86f41661f4
peterpaker48 <peterpaker611@gmail.com>
2016-04-11 11:52:08 +0800

    [travis] test

--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NGX_PATH=`pwd`'/nginx/sbin'
-`pwd`'/nginx/sbin/nginx -V'
+NGX_PATH=`pwd`'/build/nginx/sbin'
+`pwd`'/build/nginx/sbin/nginx -V'
 export PATH=${NGX_PATH}:$PATH
 prove -r t
 echo "ngx_php test success"
