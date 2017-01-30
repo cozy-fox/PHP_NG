ec5b68d8083894ac79c49feac31ea73082751553
peterpaker48 <peterpaker611@gmail.com>
2017-01-30 23:22:59 +0800

    [src] create ngx_http_php_output

---
+++ src/ngx_http_php_output.h
@@ -0,0 +1,17 @@
+/**
+ *    Copyright(c) 2016-2017 rryqszq4
+ *
+ *
+ */
+
+#ifndef NGX_HTTP_PHP_OUTPUT_H
+#define NGX_HTTP_PHP_OUTPUT_H
+
+#include <nginx.h>
+#include <ngx_http.h>
+
+void ngx_http_php_set_output(char *buffer, int buffer_len);
+
+void ngx_http_php_set_output_empty();
+
+#endif
