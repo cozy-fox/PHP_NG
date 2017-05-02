a958b0c9de156fa34fe37fd7305afa7c18838092
peterpaker48 <peterpaker611@gmail.com>
2017-05-02 17:38:59 +0800

    [src] Fixed ngx_http_php_sleep.c:303:5: error: ‘tsrm_ls’ undeclared (first use in this function)

--- src/ngx_http_php_sleep.c
+++ src/ngx_http_php_sleep.c
@@ -263,7 +263,9 @@ ngx_http_php_sleep_generator_run(ngx_http_request_t *r)
 
     ctx->rewrite_phase = 1;
     ctx->phase_status = NGX_AGAIN;
-    //TSRMLS_SET_CTX(ctx->uthread_ctx);
+
+    TSRMLS_FETCH();
+    TSRMLS_SET_CTX(ctx->uthread_ctx);
 
     ngx_memzero(&ctx->sleep, sizeof(ngx_event_t));
 
@@ -294,9 +296,9 @@ ngx_http_php_sleep_generator_handler(ngx_event_t *ev)
 
     r = ev->data;
 
-    //ngx_http_php_ctx_t *ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
+    ngx_http_php_ctx_t *ctx = ngx_http_get_module_ctx(r, ngx_http_php_module);
 
-    //TSRMLS_FETCH_FROM_CTX(ctx->uthread_ctx);
+    TSRMLS_FETCH_FROM_CTX(ctx->uthread_ctx);
 
     //ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "rewrite_phase: %d r:%p closure:%p", ctx->rewrite_phase,r,ctx->generator_closure);
 
@@ -317,10 +319,8 @@ ngx_http_php_sleep_generator_handler(ngx_event_t *ev)
 
     }zend_end_try();
 
-    //ctx->rewrite_phase = 2;
-
-    //if (ctx->rewrite_phase || ctx->access_phase || ctx->content_phase) {
+    if (ctx->phase_status == NGX_AGAIN) {
         ngx_http_core_run_phases(r);
-    //}
+    }
 }
 
